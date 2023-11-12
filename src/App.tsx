39d0bff0a789739f8ce1672016456cc31a515099
Vitaliy <ivahiv99@gmail.com>
2023-11-12 17:44:03 +0200

    feat: basic conversion logic added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
 import { useEffect, createContext } from 'react';
 import useSWR from 'swr'
 import { Converter } from './components'; 
+import styled from 'styled-components';
+
+
+const HeaderPlaceholder = styled.div`
+  height: 100px;
+  width: 100vw;
+  background-color: #999;
+  margin-bottom: 30px;
+`
+
+const ContentContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`;
 
 const mockData = [{"ccy":"CHF","base_ccy":"UAH","buy":"40.00670","sale":"40.00670"},{"ccy":"CZK","base_ccy":"UAH","buy":"1.56860","sale":"1.56860"},{"ccy":"GBP","base_ccy":"UAH","buy":"44.18370","sale":"44.18370"},{"ccy":"ILS","base_ccy":"UAH","buy":"9.37100","sale":"9.37100"},{"ccy":"JPY","base_ccy":"UAH","buy":"0.23848","sale":"0.23848"},{"ccy":"NOK","base_ccy":"UAH","buy":"3.22790","sale":"3.22790"},{"ccy":"PLZ","base_ccy":"UAH","buy":"8.65850","sale":"8.65850"},{"ccy":"SEK","base_ccy":"UAH","buy":"3.31180","sale":"3.31180"
 }];
 
-
-const ExchangeRates = createContext<any[]>([]);
+export const ExchangeRates = createContext<any[]>([]);
 
 function App() {
   const endpoint = 'https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=4';
@@ -17,11 +30,14 @@ function App() {
 
   useEffect(()=>console.log({data}), [data]);
 
+
+  // TODO: Replace context with zustand
   return (
     <ExchangeRates.Provider value={data || []}>
-      <div>
+      <HeaderPlaceholder/>
+      <ContentContainer>
         <Converter/>
-      </div>
+      </ContentContainer>
     </ExchangeRates.Provider>
   );
 }
--- src/components/Converter.tsx
+++ src/components/Converter.tsx
@@ -1,25 +1,56 @@
-import { FC } from "react";
+import { FC, useContext, useEffect, useState } from "react";
 import styled from "styled-components";
 // import Dropdown from './Dropdown';
+import {ExchangeRates} from '../App';
 
+const FlexRow = styled.div`
+    display: flex;
+    flex-direction: row;
+`
+const ConverterContainer = styled(FlexRow)`
+    background-color: red;
+`;
 
 const Converter:FC = () => {
+    const [baseInput, setBaseInput] = useState<number>(100);
+    const [translatedInput, setTranslatedInput] = useState<string>('');
+
+    const data = useContext(ExchangeRates);
+
+    console.log({data});
+
+
+    //TODO: move out
+    const convert = (value: number, currency: string, action: string) => {
+        const rate = data.find((rateItem) => rateItem.ccy === currency);
+        console.log({rate})
+        return action === 'sell' ? value / rate.sale : value / rate.buy;
+    };
+
+    useEffect(() => {
+        if(baseInput) {
+            const newTranslatedInput = convert(baseInput, 'PLZ', 'sell');
+            console.log({newTranslatedInput});
+            setTranslatedInput(newTranslatedInput.toFixed(2).toString()); 
+        }
+    }, [baseInput, data]);
+
 
     return (
-        <div>
+        <ConverterContainer>
             <div>
                 <label htmlFor="">
-                    <input type="text" />
+                    <input type="text" value={baseInput} onChange={(value) => setBaseInput(+value) }/>
                 </label>
                 {/* <Dropdown></Dropdown> */}
             </div>
             <div>
                 <label htmlFor="">
-                    <input type="text" />
+                    <input type="text" value={translatedInput}/>
                 </label>
                 {/* <Dropdown></Dropdown> */}
             </div>
-        </div>
+        </ConverterContainer>
     );
 };
 
